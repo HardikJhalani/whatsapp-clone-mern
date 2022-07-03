3ebe049cfa7cb30bd16c86066c856059e93b4e96
Hardik Jhalani <hardikjhalani23092001@gmail.com>
2022-07-03 15:05:42 +0530

    first commit

---
+++ whatsapp-mern-clone/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./sidebar";
+import Chat from "./chat";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app-body">
+        <Sidebar />
+        <Chat />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp-mern-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDzxhNnAxA-C9A-c_gMb7BPI_tkSkntZyQ",
+    authDomain: "whatsapp-mern-e9226.firebaseapp.com",
+    projectId: "whatsapp-mern-e9226",
+    storageBucket: "whatsapp-mern-e9226.appspot.com",
+    messagingSenderId: "492936110117",
+    appId: "1:492936110117:web:ca1c55300ab310e282dfcc"
+  };
