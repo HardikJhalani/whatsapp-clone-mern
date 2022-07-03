60c12ff7e833814c1b2faec1c164753530354811
Hardik Jhalani <hardikjhalani23092001@gmail.com>
2022-07-03 17:21:29 +0530

    added sidebar header

--- whatsapp-mern-clone/src/sidebar.js
+++ whatsapp-mern-clone/src/sidebar.js
@@ -1,10 +1,31 @@
-import React from 'react'
-import './sidebar.css'
+import React from "react";
+import "./sidebar.css";
+import ChatIcon from "@mui/icons-material/Chat";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { Avatar, IconButton } from "@mui/material";
 
 function Sidebar() {
   return (
-    <div className='sidebar'>Sidebar</div>
-  )
+    <div className="sidebar">
+      <div className="sidebar-header">
+        <Avatar src="https://pps.whatsapp.net/v/t61.24694-24/217815176_741784190114712_6986526512662673842_n.jpg?ccb=11-4&oh=b8c701e148608d68893987469a8d7982&oe=62D051F4" />
+        <div className="sidebar-header-right">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
